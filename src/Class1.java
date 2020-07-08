4c0a0762ec4aa5e84565073c63e1e210d606c74f
lenovo <lenovo@192.168.2.8>
2020-07-08 16:12:06 +0530

    Code is added

--- src/Class1.java
+++ src/Class1.java
@@ -1,7 +1,9 @@
 
 public class Class1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		System.out.println("hlo");
 		// TODO Auto-generated method stub
 
 	}
